d318fa6fe337ed8380a68f8537596a21553feee0
iJeeSung <kjs2395@gmail.com>
2026-01-05 22:24:42 -0800

    Added a 60 FPS limit to save CPU resources

--- main.py
+++ main.py
@@ -7,13 +7,22 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    #init game
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    #creating blank clock object
+    game_clock, dt = pygame.time.Clock(), 0
     while True:
         log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill("black")
         pygame.display.flip()
+        #adding fps
+        time_delta = game_clock.tick(60)
+        dt = time_delta / 1000
 
 if __name__ == "__main__":
     main()
